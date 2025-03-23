6e3d5bbf3e5a036b8fd90ce924c0947481db25a5
Nicholas Liem <13521135@std.stei.itb.ac.id>
2025-03-23 13:54:59 +0700

    Use brain instead of brain-controller, and create managers and integrators

--- src/brain/build_and_push.sh
+++ src/brain/build_and_push.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Set variables
-APP_NAME="brain-controller"
+APP_NAME="brain"
 REGISTRY="localhost:5000"
 IMAGE_TAG="latest"
 
 # Navigate to the directory where the Go code is located
-cd src/brain-controller || exit
+cd src/brain || exit
 
 # Build the Go binary
 echo "Building Go application..."
@@ -17,7 +17,7 @@ cd ../../
 
 # Build the Docker image
 echo "Building Docker image..."
-docker build -t $REGISTRY/$APP_NAME:$IMAGE_TAG -f src/brain-controller/Dockerfile src/brain-controller/
+docker build -t $REGISTRY/$APP_NAME:$IMAGE_TAG -f src/brain/Dockerfile src/brain/
 
 # Push the image to the local registry
 echo "Pushing Docker image to local registry..."
---
+++ src/brain/warm_pool_manager/manager.go
@@ -0,0 +1,1 @@
+package warm_pool_manager
